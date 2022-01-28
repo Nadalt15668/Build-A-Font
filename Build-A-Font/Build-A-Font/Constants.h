e59572b7c55d91cf0d769ad6784e689543577876
Nadav Altabet <nadavalt1@protonmail.com>
2022-01-28 15:07:19 +0200

    Revert "Generic "Button" Class Added"
    This reverts commit 5472fa09b5b5a62535a27d5e553746a8dcd04032.

--- Build-A-Font/Build-A-Font/Constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define WIDTH 1280
-#define HEIGHT 720
-
-#define DEFAULT_BUTTON_SIZE Vector2f(180, 90)
-
-#define GRAY_SUB 30
-#define DEFAULT_BUTTON_COLOR Color(143, 143, 143)
--- Build-A-Font/Build-A-Font/Source.cpp
+++ Build-A-Font/Build-A-Font/Source.cpp
@@ -1,18 +1,10 @@
-#include "Button.h"
+#include <SFML/Graphics.hpp>
 
-void func1(Button<>& button)
-{
-    button.SetColor(Color::Red);
-}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
-    Button<Button<>&> button(&window, Vector2f(50, 50), &func1, DEFAULT_BUTTON_SIZE, Color::Red);
-    Button<> button1(&window, Vector2f(400, 600), NULL, DEFAULT_BUTTON_SIZE);
-    Font font;
-    font.loadFromFile("font.ttf");
-    button.AddText("banana", font, 40);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -20,12 +12,9 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            button.Update(event, button1);
-            button1.Update(event);
         }
-        window.clear(Color::White);
-        button.Draw();
-        button1.Draw();
+
+        window.clear();
         window.display();
     }
 
